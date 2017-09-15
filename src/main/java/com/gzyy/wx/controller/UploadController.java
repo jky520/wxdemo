14e89b7fc6b60af66111501cd79e32a785042eff
jky520 <jky1988@qq.com>
2017-09-15 17:03:47 +0800

    新增了一些功能

--- src/main/java/com/gzyy/wx/controller/UploadController.java
+++ src/main/java/com/gzyy/wx/controller/UploadController.java
@@ -2,10 +2,7 @@ package com.gzyy.wx.controller;
 
 import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
@@ -32,12 +29,11 @@ public class UploadController {
      * @return
      */
     @GetMapping(value = "wx/index")
-    public Map<String,Object> index(Map map) {
+    public File[] index(Map map) {
         // 获取已存在文件
         File[] files = new File(uploadPath).listFiles();
         System.out.println("======"+ files);
-        map.put("files", files);
-        return map;
+        return files;
     }
 
 
@@ -46,7 +42,7 @@ public class UploadController {
      * @param file
      * @param request
      */
-    @PatchMapping("wx/upload")
+    @PostMapping("wx/upload")
     public void upload(@RequestParam("file")MultipartFile file, HttpServletRequest request) {
         // 可以获取传过来的参数
         System.out.println("name==========" + request.getParameter("name"));
--- src/main/resources/static/js/upload.js
+++ src/main/resources/static/js/upload.js
@@ -2,18 +2,36 @@
  * Created by jiang on 2017/9/15.
  */
 var vm = new Vue({
-    el:"#app",
+    el: '#app',
     data: {
+        message: 'Hello Vue!',
         files:[]
     },
+    mounted: function () {
+        this.$nextTick(function () {
+            this.getFiles();
+        })
+    },
     methods: {
-        get:function () {
-            alert("===============")
-            this.$http.get('wx/index').then(function (res) {
-                alert(res.data);
+        getFiles: function () {
+            var _this = this;
+            // ajax的get请求
+            /*$.get("/wx/index", function (r) {
+                console.log(r);
+            })*/
+            // 下面是vue-resource插件的使用
+            this.$http.get("/wx/index").then(function (res) {
+                // console.log(res.data)
+                vm.files = res.data;
             },function (res) {
-                console.log(res.status);
+                console.log(res.data)
             })
-        },
+
+           /* this.$http.get("/wx/index").then((data() => {
+                    this.successCallback();
+                },{res} => {
+
+            });*/
+        }
     }
 })
